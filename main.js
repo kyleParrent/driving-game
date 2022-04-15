fb4aa6d26ff1504f67086c169cefa01c60d031ce
Kyle Parrent <kppp433@gmail.com>
2022-04-15 22:40:31 +0000

    Completed issue 5.

--- main.js
+++ main.js
@@ -13,12 +13,16 @@ var car = {
 function moveCar(event) {
   if (event.key === 'ArrowLeft') {
     image.className = 'left';
+    car.direction = 'left';
   } else if (event.key === 'ArrowRight') {
     image.className = 'right';
+    car.direction = 'right';
   } else if (event.key === 'ArrowUp') {
     image.className = 'up';
+    car.direction = 'up';
   } else if (event.key === 'ArrowDown') {
     image.className = 'down';
+    car.direction = 'down';
   }
   if (event.key === ' ') {
     if (car.status === 'stop') {
@@ -32,10 +36,27 @@ function moveCar(event) {
 }
 
 function forward(event) {
-  var currentX = parseInt(image.style.left);
-  car.location.x = currentX;
-  car.location.x += 5;
-  image.style.left = car.location.x + 'px';
+  if (car.direction === 'right') {
+    var currentX = parseInt(image.style.left);
+    car.location.x = currentX;
+    car.location.x += 5;
+    image.style.left = car.location.x + 'px';
+  } else if (car.direction === 'left') {
+    var currentX2 = parseInt(image.style.left);
+    car.location.x = currentX2;
+    car.location.x = car.location.x - 5;
+    image.style.left = car.location.x + 'px';
+  } else if (car.direction === 'down') {
+    var currentY = parseInt(image.style.top);
+    car.location.y = currentY;
+    car.location.y += 5;
+    image.style.top = car.location.y + 'px';
+  } else if (car.direction === 'up') {
+    var currentY2 = parseInt(image.style.top);
+    car.location.y = currentY2;
+    car.location.y = car.location.y - 5;
+    image.style.top = car.location.y + 'px';
+  }
 }
 
 window.addEventListener('keydown', moveCar);
